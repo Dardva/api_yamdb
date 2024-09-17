cd1a54c783cc5f7931bcb983945c6f625e6c2f14
Ivan Danilin <ioann.vorobey1029@gmail.com>
2024-09-17 19:33:35 +0200

    configured urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
-from .views import ReviewViewSet, CommentViewSet
+from .views import (
+    CategoryViewSet, CommentViewSet, GenreViewSet, ReviewViewSet, TitleViewSet)
 
 router = DefaultRouter()
 router.register(
@@ -16,6 +17,9 @@ router.register(
     CommentViewSet,
     basename='comments'
 )
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('genres', GenreViewSet, basename='genres')
+router.register('titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
