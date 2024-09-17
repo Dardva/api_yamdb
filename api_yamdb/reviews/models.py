d8b497d80fe1ce4a070062c0e8940de581390cbe
Ivan Danilin <ioann.vorobey1029@gmail.com>
2024-09-17 16:43:25 +0200

    created models: Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from reviews.constants import MAX_NAME_LENGTH
+
+
+class Category(models.Model):
+    name = models.CharField('Название', max_length=MAX_NAME_LENGTH)
+    slug = models.SlugField('Слаг', unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    name = models.CharField('Название', max_length=MAX_NAME_LENGTH)
+    slug = models.SlugField('Слаг', unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=MAX_NAME_LENGTH)
+    year = models.IntegerField('Год создания')
+    rating = models.IntegerField('Рейтинг')
+    description = models.TextField('Описание')
+    genre = models.ForeignKey(
+        Genre, verbose_name='Жанр', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, verbose_name='Категория', on_delete=models.CASCADE)
+
+    class Meta:
+        default_related_name = 'titles'
+        ordering = ('year',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
