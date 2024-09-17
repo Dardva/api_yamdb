e2847e2688dcfeda71c983f4fa6e285371ef1117
Maksim <maksik290896@yandex.ru>
2024-09-17 20:16:12 +0500

    модели: Title(вычисление рейтинга произведения) Review(модель отзыва) Comment(модель коммента)
    api/views
    добавлен вьюсет для Review, Comment
    
    api/serializers
    добавлен сериализатор модели Title(для получения рейтинга произведения)
    сериализатор модели Review
    сериализатор модели Comment
    
    api/urls (обновил)
    добавлены маршруты для работы с отзывами и комментариями

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
+from .views import ReviewViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
 urlpatterns = [
+    path('v1/', include(router.urls)),
     path('v1/jwt/create/',
          TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
