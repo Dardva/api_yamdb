1ef2cbc39277f7c330e917ed054e21ab631e0d4b
Maksim <maksik290896@yandex.ru>
2024-09-24 20:07:43 +0500

    убран raiting c  TitleAdmin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Category, Genre, Title, Review, Comment
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug',)
@@ -8,6 +9,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('name',)
     prepopulated_fields = {'slug': ('name',)}
     ordering = ('name',)
+
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
@@ -17,9 +19,10 @@ class GenreAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     ordering = ('name',)
 
+
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'year', 'category', 'display_genres', 'rating',)
+    list_display = ('name', 'year', 'category', 'display_genres',)
     search_fields = ('name', 'category__name', 'genre__name',)
     list_filter = ('year', 'category', 'genre',)
     ordering = ('name',)
@@ -31,12 +34,14 @@ class TitleAdmin(admin.ModelAdmin):
         return ", ".join([genre.name for genre in obj.genre.all()])
     display_genres.short_description = 'Жанры'
 
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'score', 'pub_date',)
     search_fields = ('title__name', 'author__username', 'text',)
     list_filter = ('score', 'pub_date',)
     ordering = ('-pub_date',)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
